f805574ac90bf3e78ea22bae6c57943d7832d1f1
Ali Yavari <alimoien.y@gmail.com>
2026-07-02 19:38:11 +0800

    updated the python based code generator script based on Morteza new sage models and also new verification approach

--- verification/evaluate.sh
+++ verification/evaluate.sh
@@ -8,12 +8,23 @@ TIMESTAMP=$(date +"%H%M_%d%m%Y")
 OUT_DIR="${TIMESTAMP}_verification"
 echo $OUT_DIR
 
-python3 /work/run_step3_ironmask_pini_all.py \
+# python3 /work/run_step3_ironmask_pini.py \
+#   --ironmask /work/IronMask/src/ironmask \
+#   --gadgets-c /work/gadgets/gadgets.c \
+#   --out-dir /work/results/$OUT_DIR \
+#   --max-order 5 \
+#   --jobs 4 \
+#   --timeout 36000 \
+#   --include-pini \
+#   "$@"
+
+python3 /work/run_step3_ironmask_hpc2_og.py \
   --ironmask /work/IronMask/src/ironmask \
   --gadgets-c /work/gadgets/gadgets.c \
   --out-dir /work/results/$OUT_DIR \
-  --max-order 5 \
+  --max-order 3 \
   --jobs 4 \
-  --timeout 36000 \
+  --timeout 840000 \
   --include-pini \
+  --include-hpc2 \
   "$@"
